7d761cea637564bf693d84f25e63f25af5b67c0f
kayman <xmaszekx@gmail.com>
2026-09-19 22:48:16 +0200

    TF-3 prepare all projects structure with default pom files

---
+++ booking-service/src/test/java/pl/kayman/ticketflow/booking/BookingServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package pl.kayman.ticketflow.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
